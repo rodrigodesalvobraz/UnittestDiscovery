4f73b97932ff591f8fced5a7628dd19ec973e82f
rodrigodesalvobraz <rodrigobraz@gmail.com>
2024-01-15 14:13:47 -0800

    Changed root package name.

--- package/integration/test_some_integration.py
+++ package/integration/test_some_integration.py
@@ -6,4 +6,4 @@ from ..util.foo import run_foo
 class TestSomeIntegration(unittest.TestCase):
     def test_some_integration(self):
         run_foo()
-        print("Integration test")
+        print("Integration package")
--- package/unit/test_some_unit.py
+++ package/unit/test_some_unit.py
@@ -6,4 +6,4 @@ from ..util.foo import run_foo
 class TestSomeUnit(unittest.TestCase):
     def test_some_unit(self):
         run_foo()
-        print("Unit test")
+        print("Unit package")
